34b4cbc1a1d52ecf8fecdee7c40e64b704f2a7c4
Lisa Feng <lisafeng10@gmail.com>
2020-07-02 02:02:50 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -8,7 +8,7 @@ class Backer
     @backed_projects = []
   end
   
-  def back_project(project)
+  def back_project=(project)
     project = Project.new(title)
     @backed_projects << project
   end
